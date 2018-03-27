2ef71ebde5186d07d92f06e2ecd50e454d85c639
Austin <>
2018-03-27 15:44:17 -0600

    Implemented devise and view helper for authentication

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  it 'can be created' do
-    admin = Admin.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
-    expect(admin).to be_valid
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
